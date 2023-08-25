fa43f35097cd57ab114a846898003c6cd3d2fb64
shree0007 <shree.bhusal07@gmail.com>
2023-08-25 13:08:29 +0300

    added icons and favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Music APP
+      <h1>Music Player</h1>
+      <FontAwesomeIcon style={{ color: 'green', fontSize: '2rem', marginRight: '2rem' }} icon={faPlay} />
+      <FontAwesomeIcon style={{ color: 'red', fontSize: '2rem' }} icon={faPause} />
     </div>
   );
 }
